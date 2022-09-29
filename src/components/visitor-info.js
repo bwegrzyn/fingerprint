99f8f7cece74678416f8a9e09fcb96b534391ade
Bart Wegrzyn <1631754+bwegrzyn@users.noreply.github.com>
2022-09-29 17:28:58 -0400

    Initial commit with Fingerprint Pro

---
+++ src/components/visitor-info.js
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import {useVisitorData} from '@fingerprintjs/fingerprintjs-pro-react';
+
+const VisitorInfo = () => {
+  const {
+    isLoading, error, data
+  } = useVisitorData();
+
+  console.log(isLoading)
+  console.log(error)
+  console.log(data)
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>An error occurred: {error.message}</div>;
+  }
+
+  if (data) {
+    return (
+      <div>
+        <h2>Visitor Data</h2>
+        <p>{data ? data.visitorId : 'Loading...'}</p>
+        <br/>
+      </div>
+
+    );
+  } else {
+    return null;
+  }
+
+};
+
+export default VisitorInfo;
